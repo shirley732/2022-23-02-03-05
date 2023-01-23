15ae2d4089972b2d0847470ee67282fc1e5ee336
Stewart Shirley <123391202+shirley732@users.noreply.github.com>
2023-01-23 15:40:29 +0000

    completed the 2nd version

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
-// write a program that reverses a character array
-
 #include <stdio.h>
 #include <string.h>
-int main() {
 
-  char s2[] = "sampleword";
-  int i = strlen(s2);
-  while (i != -1) { //run through the word from the back
-    printf("%c",s2[i]);  //print every character
-    i--;
+int main() {
+  char s1[] = "Hello, World!";
+  int len = strlen(s1) -1;
+
+  while (len>=0) {
+    printf("%c\n", s1[len]);
+    len--;
   }
   return 0;
-  
 }
